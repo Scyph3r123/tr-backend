3427b48abd1c1d98e5f6c8f9f2ddb93290c1ce25
Deme <demeyodaniel0@gmail.com>
2023-11-07 09:14:06 +0530

    projects controller

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -6,11 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::project.project', ({ strapi }) =>  ({
-    async findOne(ctx) {
-        const { id } = ctx.params;
-        const entity = await strapi.service('api::post.project').findOne(id);
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
-        return this.transformResponse(sanitizedEntity)
-    }
-}));
+module.exports = createCoreController('api::project.project');
